02748e805055cc80185184dba31d6c56f741c6ce
legacy72 <hoperoina2016@gmail.com>
2020-03-09 15:27:27 +0500

    Split settings for local and prod

--- fitness/settings/settings.py
+++ fitness/settings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j6*6qr7c4f%k(e_hlr*!0u@50)i)i+k$*k*(*p8%ysbz*pq#hc'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
