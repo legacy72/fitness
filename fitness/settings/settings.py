29e7bb5e2439b736ac3a1c281b378e42cb517b12
legacy72 <hoperoina2016@gmail.com>
2020-03-09 17:25:31 +0500

    Add med_expert app, edit requirements.txt, add User model

--- fitness/settings/settings.py
+++ fitness/settings/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'med_expert',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'fitness.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
 }
+
 
 
 # Password validation
@@ -118,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Кастомный юзер
+AUTH_USER_MODEL = 'med_expert.User'
--- med_expert/models.py
+++ med_expert/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(
+        'Логин',
+        max_length=150,
+        unique=True,
+        validators=[AbstractUser.username_validator],
+        error_messages={'unique': 'Пользователь с таким логином уже существует.'},
+    )
+
+
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+    )
+
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+    )
+
+    middle_name = models.CharField(
+        verbose_name='Отчество',
+        max_length=150,
+        blank=True,
+    )
+
+    email = models.EmailField(
+        verbose_name='Email',
+        unique=True,
+        error_messages={'unique': 'Пользователь с таким email уже существует.'},
+    )
+
+    date_of_birth = models.DateField(
+        verbose_name='Дата рождения',
+    )
+
+    GENDER_CHOICES = (
+        ('M', 'Мужской'),
+        ('F', 'Женский'),
+    )
+    gender = models.CharField(
+        verbose_name='Пол', 
+        max_length=2, 
+        choices=GENDER_CHOICES
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        indexes = [
+            models.Index(fields=['username', 'first_name', 'last_name', 'middle_name']),
+        ]
+
+
+    def __str__(self):
+        return self.username
