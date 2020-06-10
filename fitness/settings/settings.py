76c1126dc6db4fd7a3b129d786bb69f1731b53e6
Vlad <rakin_vk@smart-transport.ru>
2020-06-10 17:29:56 +0500

    Add paginator

--- fitness/settings/settings.py
+++ fitness/settings/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
